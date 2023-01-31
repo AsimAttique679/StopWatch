1477d0d0087905b1af3cf4049e2d3f429462c27e
Asimjutt690 <anokhajutt679@gmail.com>
2023-01-31 00:49:24 -0800

    StopWatch

---
+++ index.js
@@ -0,0 +1,48 @@
+// let display= document.getElementById('time')
+// let button=document.querySelectorAll('button')
+
+// let hr=0;
+// let min=0;
+// let sec=0;
+// let interval;
+ 
+// function formatNumber(digit){
+//   if(digit <10){
+//   return('0' + digit)
+// }else{
+//    return digit; 
+// }
+// }
+
+
+//  function startTimer(){
+// interval =setInterval(watch,1000)
+//  }
+
+//  function stopTimer(){
+        
+//      clearInterval(interval);
+// }
+// function resetTimer(){
+//     clearInterval(interval);
+//     sec=0
+//     min=0
+//     hr=0
+//     display.innerHTML="00:00:00"
+// }
+
+// function watch(){
+//     sec++
+//     if(sec == 60){
+//         min++
+//         sec=0
+//         if(min == 60){
+//             min=00;
+//             hr++
+//         }    
+//     }
+// // console.log(elements);
+ 
+// display.innerHTML = `${formatNumber (hr)}:${formatNumber (min)}:${formatNumber (sec)}`
+
+// }
